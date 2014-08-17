gobbledegook/creevey

commit f7dfb428228de1f8cc689052d843e1e8ae402663

version 1.3 (build 12) for OS X 10.6 and later

DYImageCache.m
@@ -110,8 +110,12 @@ NSString *FileSize2String(unsigned long long fileSize) {
 	
 	NSSize maxSize = boundingSize;
 	NSImage *orig, *result = nil;
+
+	// use this if you're targeting 10.6 or later
+	orig = [[NSImage alloc] initWithDataIgnoringOrientation:[NSData dataWithContentsOfFile:ResolveAliasToPath(imgInfo->path)]];
+
 	// calc pixelSize
-	orig = [[NSImage alloc] initByReferencingFile:ResolveAliasToPath(imgInfo->path)];
+	//orig = [[NSImage alloc] initByReferencingFile:ResolveAliasToPath(imgInfo->path)];
 	   /* You MUST either setDataRetained:YES OR setCacheMode:NSImageCacheNever.
 		* Once the image is composited, NSImage will throw away the original
 		* data and save only a cached (read: reduced) version. It seems that

Info.plist
@@ -106,7 +106,7 @@
 	<key>CFBundleExecutable</key>
 	<string>Phoenix Slides</string>
 	<key>CFBundleGetInfoString</key>
-	<string>Phoenix Slides 1.2.8, Copyright 2005-2012 Dominic Yu, Gold Mountain Software</string>
+	<string>Phoenix Slides 1.3i, Copyright 2005-2014 Dominic Yu, Gold Mountain Software</string>
 	<key>CFBundleHelpBookFolder</key>
 	<string>Phoenix Slides Help</string>
 	<key>CFBundleHelpBookName</key>
@@ -122,13 +122,13 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>1.2.8</string>
+	<string>1.3i</string>
 	<key>CFBundleSignature</key>
 	<string>CrvY</string>
 	<key>CFBundleVersion</key>
-	<string>11</string>
+	<string>12</string>
 	<key>NSHumanReadableCopyright</key>
-	<string>©2005-2012 Dominic Yu, Gold Mountain Software.</string>
+	<string>©2005-2014 Dominic Yu, Gold Mountain Software.</string>
 	<key>NSMainNibFile</key>
 	<string>MainMenu</string>
 	<key>NSPrincipalClass</key>

commit d751b8f90a9ca41dd105354e45f012cc47314149

fix display of jpeg orientations and some raw files (broken in f7dfb42)

CreeveyController.h
@@ -12,7 +12,6 @@
 
 @class CreeveyMainWindowController, DYImageCache, SlideshowWindow, DYJpegtranPanel;
 
-BOOL FileIsJPEG(NSString *s);
 NSMutableAttributedString* Fileinfo2EXIFString(NSString *origPath, DYImageCache *cache, BOOL moreExif);
 
 #define NUM_FNKEY_CATS 11

CreeveyController.m
@@ -24,12 +24,6 @@
 #define DYVERSCHECKINTERVAL 604800
 #define MAX_FILES_TO_CHECK_FOR_JPEG 100
 
-BOOL FileIsJPEG(NSString *s) {
-	return [[[s pathExtension] lowercaseString] isEqualToString:@"jpg"]
-	|| [[[s pathExtension] lowercaseString] isEqualToString:@"jpeg"]
-	|| [NSHFSTypeOfFile(s) isEqualToString:@"JPEG"];
-}
-
 BOOL FilesContainJPEG(NSArray *paths) {
 	// find out if at least one file is a JPEG
 	if ([paths count] > MAX_FILES_TO_CHECK_FOR_JPEG) return YES; // but give up there's too many to check

DYCarbonGoodies.h
@@ -17,3 +17,10 @@ NSString *ResolveAliasToPath(NSString *path);
 
 // check if a files invisible flag is set
 BOOL FileIsInvisible(NSString *path);
+
+BOOL FileIsJPEG(NSString *s);
+
+@interface NSImage (DYCarbonGoodies)
++ (instancetype)imageByReferencingFileIgnoringJPEGOrientation:(NSString *)fileName;
+- (instancetype)initByReferencingFileIgnoringJPEGOrientation:(NSString *)fileName;
+@end

DYCarbonGoodies.m
@@ -32,3 +32,25 @@ BOOL FileIsInvisible(NSString *path) {
 	
 	return (info.flags & kLSItemInfoIsInvisible) != 0;
 }
+
+BOOL FileIsJPEG(NSString *s) {
+	return [[[s pathExtension] lowercaseString] isEqualToString:@"jpg"]
+	|| [[[s pathExtension] lowercaseString] isEqualToString:@"jpeg"]
+	|| [NSHFSTypeOfFile(s) isEqualToString:@"JPEG"];
+}
+
+@implementation NSImage (DYCarbonGoodies)
+
++ (instancetype)imageByReferencingFileIgnoringJPEGOrientation:(NSString *)fileName
+{
+	return [[[NSImage alloc] initByReferencingFileIgnoringJPEGOrientation:fileName] autorelease];
+}
+
+- (instancetype)initByReferencingFileIgnoringJPEGOrientation:(NSString *)fileName
+{
+	if (FileIsJPEG(fileName)) return [self initWithDataIgnoringOrientation:[NSData dataWithContentsOfFile:fileName]];
+	// initWithDataIgnoringOrientation: doesn't seem to create image representations for some (raw?) files
+	return [self initByReferencingFile:fileName];
+}
+
+@end

DYImageCache.m
@@ -109,7 +109,7 @@ NSString *FileSize2String(unsigned long long fileSize) {
 	NSSize maxSize = boundingSize;
 	NSImage *orig, *result = nil;
 
-	orig = [[NSImage alloc] initWithDataIgnoringOrientation:[NSData dataWithContentsOfFile:ResolveAliasToPath(imgInfo->path)]];
+	orig = [[NSImage alloc] initByReferencingFileIgnoringJPEGOrientation:ResolveAliasToPath(imgInfo->path)];
 
 	// now scale the img
 	if (orig && [[orig representations] count]) { // why doesn't it return nil for corrupt jpegs?

SlideshowWindow.m
@@ -451,7 +451,7 @@ scheduledTimerWithTimeInterval:timerIntvl
 		if (zoomInfo || ([imgView showActualSize] &&
 						 !(info->pixelSize.width < [imgView bounds].size.width &&
 						   info->pixelSize.height < [imgView bounds].size.height))) {
-			[imgView setImage:[[[NSImage alloc] initByReferencingFile:ResolveAliasToPath(theFile)] autorelease]
+			[imgView setImage:[NSImage imageByReferencingFileIgnoringJPEGOrientation:ResolveAliasToPath(theFile)]
 					   zoomIn:2];
 			if (zoomInfo) [imgView setZoomInfo:zoomInfo];
 		}
@@ -784,8 +784,7 @@ scheduledTimerWithTimeInterval:timerIntvl
 			if ((obj = [imgCache infoForKey:[filenames objectAtIndex:currentIndex]])) {
 				if (obj->image == [imgView image]
 					&& !NSEqualSizes(obj->pixelSize,[obj->image size])) { // cached image smaller than orig
-					[imgView setImage:[[[NSImage alloc] initByReferencingFile:
-						ResolveAliasToPath([filenames objectAtIndex:currentIndex])] autorelease]
+					[imgView setImage:[NSImage imageByReferencingFileIgnoringJPEGOrientation:ResolveAliasToPath([filenames objectAtIndex:currentIndex])]
 							   zoomIn:c == '=' ? 2 : c == '+'];
 				} else {
 					if (c == '+') [imgView zoomIn];
@@ -829,8 +828,7 @@ scheduledTimerWithTimeInterval:timerIntvl
 			if ((obj = [imgCache infoForKey:[filenames objectAtIndex:currentIndex]])) {
 				if (obj->image == [imgView image]
 					&& !NSEqualSizes(obj->pixelSize,[obj->image size])) {  // cached image smaller than orig
-					[imgView setImage:[[[NSImage alloc] initByReferencingFile:
-						ResolveAliasToPath([filenames objectAtIndex:currentIndex])] autorelease]
+					[imgView setImage:[NSImage imageByReferencingFileIgnoringJPEGOrientation:ResolveAliasToPath([filenames objectAtIndex:currentIndex])]
 							   zoomIn:c == '=' ? 2 : c == '+'];
 				} else {
 					if (c == '+') [imgView zoomIn];